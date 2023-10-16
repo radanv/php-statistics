59539c8d864aeb092f9ab6f9e7623429b975c596
radanv <vladriordan@gmail.com>
2023-10-16 16:19:32 -0400

    v1.0.1

---
+++ core/admin-addBD.php
@@ -0,0 +1,126 @@
+<?php /*  
+session_start();
+if($_SESSION["valid"] != "true"){
+header("Location: http://statistics.services.hosting-israel.co.il/login.php"); 
+exit;
+}*/
+?>
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title> Statistics Control </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel='stylesheet' type="text/css" href='../includes/css/style.css'>
+  </head>
+<noindex>
+<body>
+<div class="form-create-bd">
+    <form action="" method="POST" style="" enctype=multipart/form-data>
+            <input type="text"      name="Name"          placeholder="Name"/>
+            <input type="text"      name="Password"      placeholder="Password"/>
+            <input type="text"      name="Phone"         placeholder="Phone"/>
+            <input type="text"      name="Email"         placeholder="email"/>
+            <input type="text"      name="Budget"        placeholder="Budget"/>
+            <input type="text"      name="Interest"         placeholder="Interest"/>
+            <input type="text"      name="Visitors"      placeholder="Visitors"/>
+            <input type="text"      name="Views"         placeholder="Views"/>
+            <input type="text"      name="Domain"          placeholder="Domain"/>
+            <input type="text"      name="PeriodFrom"    placeholder="2017-01-01"/>
+            <input type="text"      name="PeriodUntil"   placeholder="2020-12-01"/>
+	        <input type="submit"    name="addBD" value="Save" />
+    </form>
+</div>
+
+    <!--   УДАЛЕНИЕ ИЗ БАЗЫ                --> 
+    <div id="del-form">
+        <form action="" method="get">
+            <input name="delete" type="text" size="10" placeholder="Articles">
+            <input type="submit" id="delBtn" value="УДАЛИТЬ">
+        </form>
+        <span style="padding: 5px; display: block; background: #FFDDDD;">Для удаления впишите номер статьи (например 193).</span>
+    </div>
+<?php   
+header('Content-type: text/html; charset=utf-8');
+
+$sec = 3;
+$page = "http://statistics.services.hosting-israel.co.il/core/admin-addBD.php";
+
+$mysql_host = ""; // sql сервер
+$mysql_user = ""; // пользователь
+$mysql_password = ""; // пароль
+$mysql_database = ""; // имя базы данных c
+
+$db_ad = mysql_connect($mysql_host, $mysql_user, $mysql_password); // коннект к серверу SQL
+mysql_select_db($mysql_database); // коннект к БД на сервере
+mysql_query('SET NAMES utf8');
+mysql_query('SET CHARACTER SET utf8' );
+mysql_query('SET COLLATION_CONNECTION="utf8_general_ci"' );
+
+if (isset($_POST['addBD'])){
+    $Name =  trim($_POST['Name']);
+    $Password =  trim($_POST['Password']);
+    $Phone =  trim($_POST['Phone']);
+    $Email =  trim($_POST['Email']);
+    $Budget =  trim($_POST['Budget']);
+    $Interest =  trim($_POST['Interest']);
+    $Visitors =  trim($_POST['Visitors']);
+    $Views =  trim($_POST['Views']);
+    $Domain =  trim($_POST['Domain']);
+    $PeriodFrom =  trim($_POST['PeriodFrom']);
+    $PeriodUntil =  trim($_POST['PeriodUntil']);
+    
+    $strSQL = "INSERT INTO `st-clients-statistic` (Name, Password, Phone, Email, Budget, Interest, Visitors, Views, Domain, PeriodFrom, PeriodUntil) 
+               VALUES('$Name', '$Password', '$Phone', '$Email', '$Budget', '$Interest', '$Visitors', '$Views', '$Domain', '$PeriodFrom', '$PeriodUntil')"; 
+    
+    	mysql_query($strSQL); 
+    	header("Refresh: $sec; url=$page");
+}
+if($ID=$_GET['delete']){
+    $strSQL = "DELETE FROM `st-clients-statistic` WHERE id = '$ID'";
+    	mysql_query($strSQL);
+    	header("Refresh: $sec; url=$page");
+}
+/* Show me all post from base */
+$result = mysql_query("SELECT * from `st-clients-statistic` ORDER BY id DESC");
+ while ($row = mysql_fetch_assoc($result))
+{
+echo 
+"<div class=\"info-left-position\">
+    <div class=\"info-blocks left-position\">
+        <span class=\"textInfo\"> <b>id:</b> "                  .$row['id']."<br></span>
+        <span class=\"textInfo\"> <b>Name:</b>"                 .$row['Name']."<br></span>
+        <span class=\"textInfo\"> <b>Password:</b>"             .$row['Password']."<br></span>
+        <span class=\"textInfo\"> <b>Phone:</b>"                .$row['Phone']."<br></span>
+        <span class=\"textInfo\"> <b>Email:</b>"                .$row['Email']."<br></span>
+        <span class=\"textInfo\"> <b>Budget: </b>"              .$row['Budget']."<br></span>
+        <span class=\"textInfo\"> <b>Interest:</b>"             .$row['Interest']."<br></span>
+        <span class=\"textInfo\"> <b>Visitors: </b>"            .$row['Visitors']."<br></span>
+        <span class=\"textInfo\"> <b>Views:</b>"                .$row['Views']."<br></span>
+        <span class=\"textInfo\"> <b>Domain:</b>"               .$row['Domain']."<br></span>
+        <span class=\"textInfo\"> <b>PeriodFrom:</b>"           .$row['PeriodFrom']."<br></span>
+        <span class=\"textInfo\"> <b>PeriodUntil:</b>"          .$row['PeriodUntil']."<br></span>
+    </div>
+</div>";
+}
+$result2 = mysql_query("SELECT * FROM `st-visitors` ORDER BY `date` DESC");
+ while ($row2 = mysql_fetch_assoc($result2))
+{
+echo 
+"<div class=\"info-right-position\">
+    <div class=\"info-blocks right-position\">
+        <span class=\"textInfo\"> <b>id:</b> "                  .$row2['id']."<br></span>
+        <span class=\"textInfo\"> <b>Date:</b>"                 .$row2['date']."<br></span>
+        <span class=\"textInfo\"> <b>Hosts:</b>"             .$row2['hosts']."<br></span>
+        <span class=\"textInfo\"> <b>Views:</b>"                .$row2['views']."<br></span>
+        <span class=\"textInfo\"> <b>Domain:</b>"                .$row2['domain']."<br></span>
+        <span class=\"textInfo\"> <b>Link: </b>"              .$row2['link']."<br></span>
+        <span class=\"textInfo\"> <b>Ip:</b>"             .$row2['ip_address']."<br></span>
+    </div>
+</div>";
+}
+mysql_close($db_ad);
+?>
+</noindex>
+</body>
+</html>
---
+++ core/users-registration.php
@@ -0,0 +1,182 @@
+<?php 
+ob_start();
+session_start();
+header('Content-type: text/html; charset=utf-8');
+$serverName = $_SERVER['SERVER_NAME'];
+$registrationPageUrl = $serverName."/client-registration-form.php";
+$currentURL = "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+if($currentURL == $registrationPageUrl) {
+    if(isset($_POST['client-login']) && is_numeric($_POST['Phone']) && !empty($_POST['Password'])){ 
+        $clientPhone = $_POST['Phone'];
+        $clientPassword = $_POST['Password'];
+        if (strlen($clientPhone) >7 && strlen($clientPhone) < 13){
+            #if we need to create a user or login
+            if (strlen($clientPassword) > 7){
+                dataBaseConn();
+                $page = "/?phone=$clientPhone";
+                $loginResult = mysql_query("SELECT * from `st-clients` WHERE ClientPhone = '$clientPhone'");
+                $row = mysql_fetch_assoc($loginResult); 
+                if ($clientPhone == $row['ClientPhone'] ){
+                	if ($clientPassword == $row['ClientPassword']){
+                		$validationResult = 'Successful Login! The page will be reloaded in 3 seconds.';
+                		header("Refresh: 3; url=$page");
+                	}else{
+                		$validationResult = 'Wrong password!';
+            		}
+                }else{
+            		$strSQL = "INSERT INTO `st-clients` (ClientPhone, ClientPassword) 
+            		VALUES('$clientPhone','$clientPassword')"; 
+            		$validationResult = 'Successful Registration! The page will be reloaded in 3 seconds.';
+            		header("Refresh: 3; url=$page");
+            		}
+                    dataBaseConnClose();
+            }else{
+                $validationResult = 'Your password is too easy';
+            }
+        }else{
+            $validationResult = "Your phone number must be real";
+        }
+    }else{
+        $validationResult = "Log in or Sign up";
+    }
+}
+else{ 
+    #if this is a new session for a new user - show nothing ->
+    if($_GET['phone']) {
+        $clientPhone = $_GET['phone'];
+        $validationResult = "Your account $clientPhone";
+    ###########################################################################
+    //Data from DataBase for 1 month only
+    ###########################################################################
+        if (strlen($clientPhone) >7 && strlen($clientPhone) <13 ) {
+        
+        dataBaseConn();
+        
+        $dataResult = mysql_query("SELECT * from `st-clients-statistic` WHERE Phone = '$clientPhone' ORDER BY id DESC");
+        while ($row = mysql_fetch_assoc($dataResult))
+            {
+            $id = $row['id'];
+            $name = $row['Name'];
+            $Password = $row['Password'];
+            $Phone = $row['Phone'];
+            $Email = $row['Email'];
+            $Budget = $row['Budget'];
+            $Interest = $row['Interest'];
+            $Visitors = $row['Visitors'];
+            $Views = $row['Views'];
+            $Domain = $row['Domain'];
+            $PeriodFrom = $row['PeriodFrom'];
+            $PeriodUntil = $row['PeriodUntil'];
+            ;}
+            
+    ###########################################################################
+    $date = date("Y-m-d");
+        // get info about visitors from database
+    	$getStatsData = mysql_query("SELECT * FROM `st-visitors` ORDER BY `date` DESC LIMIT 6");
+    	// make a list for table
+    	//for($i=0; $i<5; $i++){
+    	
+    		while ($row = mysql_fetch_assoc($getStatsData))
+    	    {
+        		$displayStatsData .= '<tr>
+        		<td>' . $row['date'] . '</td>
+        		<td>' . $row['hosts'] . '</td>
+        		<td>' . $row['views'] . '</td>
+        		<td><details><summary>Details</summary>' . $row['domain'] . '</details></td>
+        		</tr>';
+    	//	}
+    	}
+        dataBaseConnClose();
+        }else{
+            $validationResult = 'Your phone number must be real';
+        }
+    }else{
+        $validationResult = "Log in to check your stats";
+    	$id = 0;
+    	$name = 0;
+    	$Password = 0;
+    	$Phone = 0;
+    	$Email = 0;
+    	$Budget = 0;
+    	$Interest = 0;
+    	$Visitors = 0;
+    	$Views = 0;
+    	$Domain = 0;
+    	$PeriodFrom = 0;
+    	$PeriodUntil = 0;
+    }
+}
+
+
+function dataBaseConn(){
+    //global $var;  
+    //if("Condition"){
+    //    $var = "01-01-11";
+    //}
+	$stcntr_host = ""; // sql server
+	$stcntr_user = ""; // user login
+	$stcntr_password = ""; // password
+	$stcntr_database = ""; // database name
+	
+	$db_stcntr = mysql_connect($stcntr_host, $stcntr_user, $stcntr_password) or die ("cant connect to data"); // Database SQL connection
+	mysql_select_db($stcntr_database); // server side
+	mysql_query('SET NAMES utf8');
+	mysql_query('SET CHARACTER SET utf8' );
+	mysql_query('SET COLLATION_CONNECTION="utf8_general_ci"' );
+}
+function dataBaseConnClose(){
+    mysql_close($db_stcntr);
+}
+
+function dataVisitors(){
+    dataBaseConn();
+    // Get visitors ip and date	
+    $visitor_ip = $_SERVER['REMOTE_ADDR'];
+    $date = date("Y-m-d");
+    $domain = $_SERVER['SERVER_NAME'];
+    $link = $_SERVER['REQUEST_URI'];
+    // check in database today statistic   
+    $statResult = mysql_query("SELECT * FROM `st-visitors` WHERE `date`='$date'") ;
+    $row = mysql_fetch_assoc($statResult);
+    // IF not visited today
+    if (mysql_num_rows($statResult) == 0)
+    {
+        // add +1 one host and +1 hit
+        //mysql_query("INSERT `st-visitors` SET `date`='$date',`hosts`=1,`views`=1,`domain`='$domain',`link`='$link',`ip_address`='$visitor_ip'");
+    	mysql_query("INSERT `st-visitors` SET 
+    	    `date`='$date',
+    	    `hosts`=1,
+    	    `views`=1,
+    	    `domain`= '$domain',
+    	    `link`= '$link',
+    	    `ip_address`= '$visitor_ip'");
+    }
+    else
+    {   
+        $arrayVisitor_ip = explode(", ",$row['ip_address']);
+        $arrayDomain = explode(", ",$row['domain']);
+        $arrayLink = explode(", ",$row['link']);
+        
+        if (!in_array($domain, $arrayDomain)){
+            $domain = $row['domain'].", ".$domain;
+            mysql_query("UPDATE `st-visitors` SET `domain`='$domain' WHERE `date`='$date'");
+            echo $domain;
+        }
+        if (!in_array($link, $arrayLink)){
+            $link = $row['link'].", ".$link;
+            mysql_query("UPDATE `st-visitors` SET `link`='$link' WHERE `date`='$date'");
+        }
+        // if this user already was here
+        if (in_array($visitor_ip, $arrayVisitor_ip)){
+            mysql_query("UPDATE `st-visitors` SET `views`=`views`+1 WHERE `date`='$date'");
+        }
+        else
+        {
+            $visitor_ip = $row['ip_address'].",".$visitor_ip;
+            mysql_query("UPDATE `st-visitors` SET `hosts`=`hosts`+1,`views`=`views`+1, `ip_address`='$visitor_ip' WHERE `date`='$date'");
+        }
+    }
+    dataBaseConnClose();
+}
+dataVisitors();
+?>
